41b81ba60af5bcc7a9a3e0d4ce8f2b0e16244052
Phát Nhâm <phattannham@gmail.com>
2019-09-13 09:05:43 +0700

    fix: rename migration

--- database/migrations/2019_09_12_215212_rename_all_tables.php
+++ database/migrations/2019_09_12_215212_rename_all_tables.php
@@ -12,7 +12,7 @@ class RenameAllTables extends Migration
      */
     public function up()
     {
-        $from = array('biennhan' , 'biennhan_trangthai', 'diemdanh', 'khachhang', 'khoahoc', 'lophoc', 'lophoc_danhsach', 'lophoc_nhatky', 'nhanvien', 'nhatky', 'phieuchi', 'phieuthu', 'phieuthu_danhmuc', 'taive');
+        $from = array('BIENNHAN' , 'BIENNHAN_TRANGTHAI', 'DIEMDANH', 'KHACHHANG', 'KHOAHOC', 'LOPHOC', 'LOPHOC_DANHSACH', 'LOPHOC_NHATKY', 'NHANVIEN', 'NHATKY', 'PHIEUCHI', 'PHIEUTHU', 'PHIEUTHU_DANHMUC', 'TAIVE');
         $to = array('_case' , '_case_status', '_attendance', '_client', '_course', '_class', '_class_list', '_class_log', '_staff', '_case_log', '_payment', '_receipt', '_receipt_cate', '_download');
         for($i=0; $i<count($from); $i++) {
             Schema::rename($from[$i], $to[$i]);
